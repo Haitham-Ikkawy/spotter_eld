10daeff6193f357a878b40e9e888a4c762898b9c
HaithamIkkawi <h.ikkawy@numbase.com>
2025-03-10 14:32:22 +0200

    add componenerts

--- frontend/eld-frontend/vite.config.js
+++ frontend/eld-frontend/vite.config.js
@@ -1,13 +1,13 @@
 import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
-import getConfig from './src/config/env/config.main.js';
+// import getConfig from './src/config/env/config.main.js';
 
 export default defineConfig(({mode}) => {
     // Load environment variables (optional, if needed)
     const env = loadEnv(mode, process.cwd());
-
-    // Get dynamic configuration based on the mode
-    const config = getConfig(mode);
+    //
+    // // Get dynamic configuration based on the mode
+    // const config = getConfig(mode);
 
 
     return {
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
         server: {
             proxy: {
                 '/api': {
-                    target: config.serverConfig.apiBaseUrl,
+                    target: "",
                     changeOrigin: true,
                     secure: false,
                 },
